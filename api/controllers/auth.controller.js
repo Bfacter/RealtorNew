708ec88b91f78d4e593ebf4fe1a98caaf24cc734
Bfacter <bhawanshbaleja@gmail.com>
2024-05-09 20:33:43 +0530

    create singnin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
-
+import jwt from "jsonwebtoken";
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
 
@@ -11,6 +11,31 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json("User created");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User not found"));
+    }
+    const validPassword = await bcrypt.compareSync(
+      password,
+      validUser.password
+    );
+    if (!validPassword) {
+      return next(errorHandler(400, "Wrong credentials"));
+    }
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .cookie("access_token", token, { httponly: true })
+      .status(200)
+      .json(validUser);
   } catch (error) {
     next(error);
   }
