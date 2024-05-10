6b1522f8c954421a33ad0a3157e15a2b8b1e7860
Bfacter <bhawanshbaleja@gmail.com>
2024-05-10 13:22:59 +0530

    update your listing functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import Profile from "./pages/Profile";
 import Header from "./assets/components/Header";
 import PrivateRoute from "./assets/components/PrivateRoute";
 import CreateListing from "./pages/CreateListing";
+import UpdateListing from "./pages/UpdateListing";
 export default function App() {
   return (
     <BrowserRouter>
@@ -21,6 +22,7 @@ export default function App() {
           <Route path="/profile" element={<Profile />} />
           <Route path="/create-listing" element={<CreateListing />} />
         </Route>
+        <Route path="/update-listing/:listingId" element={<UpdateListing />} />
       </Routes>
     </BrowserRouter>
   );
