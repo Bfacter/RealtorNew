586e0cadc6bb2326894d2d43572f0baaba40ead2
Bfacter <bhawanshbaleja@gmail.com>
2024-05-10 11:12:34 +0530

    mkae the profile page private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
 import Header from "./assets/components/Header";
+import PrivateRoute from "./assets/components/PrivateRoute";
 export default function App() {
   return (
     <BrowserRouter>
@@ -15,7 +16,9 @@ export default function App() {
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/about" element={<About />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
